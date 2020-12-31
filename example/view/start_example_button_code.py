5a921701d9e9d6ac484cdf29a8827fe5065c2cc5
木豆 <1540235670@qq.com>
2020-12-31 11:22:56 +0800

    button 添加示例

--- example/view/start_example_button_code.py
+++ example/view/start_example_button_code.py
@@ -1,13 +1,12 @@
 # coding=utf-8
 """
     create by pymu
-    on 2020/12/18
-    at 17:14
-    使用eq 开发一个测试页面代码
+    on 2020/12/31
+    at 9:37
+    使用EQ开发按钮示例
 """
-
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGroupBox, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QGroupBox, QPushButton, QWidget, QHBoxLayout
 
 from common.base.button import BaseButton, QPushButtonToBaseButton
 from common.base.launch import EasyQtInit
@@ -18,16 +17,19 @@ from view.activity.activity_frame_less_window_hint import FrameLessWindowHintAct
 from view.frame.frame_bar_demo0 import FrameBarDemo0
 
 
-class TestActivity(FrameLessWindowHintActivity):
+# noinspection PyAttributeOutsideInit
+class StartExampleButtonActivity(FrameLessWindowHintActivity):
+    """在代码中添加button"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.is_a = True
+        # 按钮是否可用
+        self.button5_enable = True
         self.procedure()
 
     def place(self):
-        """放置布局"""
-        super(TestActivity, self).place()
+        """放置按钮"""
+        super(StartExampleButtonActivity, self).place()
         # 添加默认的标题栏0
         self.bar: FrameBarDemo0 = FrameBarDemo0(self)
         self.body_layout.addWidget(self.bar, alignment=Qt.AlignTop)
@@ -35,13 +37,8 @@ class TestActivity(FrameLessWindowHintActivity):
         f_layout = FlowLayout(spacing=10)
         g.setLayout(f_layout)
 
-        # comBox = QComboBox()
-        # comBox.setView(QListView())
-        # comBox.addItems(
-        #     ['Java', 'C#', 'PHP', 'Java', 'C#', 'PHP', 'Java', 'C#', 'PHP', 'Java', 'C#', 'PHP', 'Java', 'C#', 'PHP', ])
-        # f_layout.addWidget(comBox)
-
         button1 = BaseButton("原始button")
+        # 类似的set 推荐放在configure中配置更好管理哦（如果配置方法很长，应该做出分类，抽离出方法，如设置button一类， set_button_configure）
         button1.setToolTip("常用于表示不引人注意的动作")
         f_layout.addWidget(button1)
 
@@ -57,61 +54,73 @@ class TestActivity(FrameLessWindowHintActivity):
 
         button4 = BaseButton("危险button")
         button4.setToolTip("带有冲击性的颜色")
+        # noinspection PyUnresolvedReferences
         button4.clicked.connect(self.unused_button5)
         button4.setProperty(*WidgetProperty.btn_class_danger[1])
         f_layout.addWidget(button4)
 
         self.button5 = BaseButton("禁用button")
-        self.button5.setToolTip("按钮不可用")
+        self.button5.setToolTip("按钮不可用(BaseButton)")
+        # 这里提倡事件连接放在 set_signal 方法中统一配置，这样好管理哦。
+        # noinspection PyUnresolvedReferences
         self.button5.clicked.connect(lambda: NormalDialogActivity(info="当前按钮是可用的").exec_())
         self.button5.setProperty(*WidgetProperty.btn_class_normal[1])
-        self.button5.setDisabled(self.is_a)
+        self.button5.setDisabled(self.button5_enable)
         f_layout.addWidget(self.button5)
 
         button6 = BaseButton("带图标的button")
-        button6.setToolTip("带图标的button")
+        button6.setToolTip("带图标的button(BaseButton)")
         button6.setProperty(*WidgetProperty.btn_class_normal[1])
         button6.setIcon(self.resource.awesome_font_icon("fa.eercast"))
         f_layout.addWidget(button6)
 
         button7 = BaseButton("可变图标的button")
-        button7.setToolTip("可变图标的button")
+        button7.setToolTip("可变图标的button(BaseButton)")
         button7.setProperty(*WidgetProperty.btn_class_normal[1])
         button7.setIcon(self.resource.awesome_font_icon("fa.frown-o"))
         button7.setListenHover(True)
+        # noinspection PyUnresolvedReferences
         button7.hover_in.connect(lambda: button7.setIcon(self.resource.awesome_font_icon("fa.grav")))
+        # noinspection PyUnresolvedReferences
         button7.hover_out.connect(lambda: button7.setIcon(self.resource.awesome_font_icon("fa.frown-o")))
         f_layout.addWidget(button7)
 
-        # todo 父类转子类
         self.button8 = QPushButton()
         self.button8.setIcon(self.resource.awesome_font_icon("fa.frown-o"))
         self.button8.setToolTip("ui加载的button， 然后升格为派生子类BaseButton， 也就是说我现在是BaseButton类型")
         f_layout.addWidget(self.button8)
 
-        # todo 父类转子类
-        button9 = QPushButton()
-        button9.setToolTip("ui加载的button")
+        # button9 = QPushButton()
+        # button9.setIcon(self.resource.awesome_font_icon("fa.upload", color="black"))
+        # button9.setToolTip("图标按钮：上传")
+        # f_layout.addWidget(button9)
+        #
+        # button10 = QPushButton()
+        # button10.setIcon(self.resource.awesome_font_icon("fa.download", color="black"))
+        # button10.setToolTip("图标按钮：上传")
+        # f_layout.addWidget(button10)
 
-        line_edit = QLineEdit("这是一个输入框")
-        f_layout.addWidget(line_edit)
+        # button 组
+        button_group = QWidget()
+        button_group_layout = QHBoxLayout(button_group)
+        button_group_layout.setContentsMargins(0, 0, 0, 0)
+        button_group_layout.setSpacing(1)
+        button11 = QPushButton()
+        button11.setIcon(self.resource.awesome_font_icon("fa.chevron-left", color="black"))
+        button11.setToolTip("快退")
+        button_group_layout.addWidget(button11)
 
-        line_edit1 = QLineEdit()
-        line_edit1.setPlaceholderText("提示输入文本")
-        f_layout.addWidget(line_edit1)
+        button12 = QPushButton()
+        button12.setIcon(self.resource.awesome_font_icon("fa.pause", color="black"))
+        button12.setToolTip("暂停")
+        button_group_layout.addWidget(button12)
 
-        # 验证失败的
-        line_edit2 = QLineEdit()
-        line_edit2.setEchoMode(QLineEdit.Password)
-        line_edit2.setPlaceholderText("输入一些文本看看")
-        f_layout.addWidget(line_edit2)
+        button13 = QPushButton()
+        button13.setIcon(self.resource.awesome_font_icon("fa.chevron-right", color="black"))
+        button13.setToolTip("快进")
+        button_group_layout.addWidget(button13)
 
-        # 验证失败的
-        line_edit3 = QLineEdit()
-        line_edit3.setPlaceholderText("假设这是你输入错误的文本")
-        line_edit3.setProperty(*WidgetProperty.border_class_red[1])
-        # line_edit3.setProperty("property_name", "border_class_blue")
-        f_layout.addWidget(line_edit3)
+        f_layout.addWidget(button_group)
 
         self.body_layout.addWidget(g)
         # 拉伸填充置顶
@@ -119,23 +128,28 @@ class TestActivity(FrameLessWindowHintActivity):
 
     def unused_button5(self):
         """设置button5不可用于可用"""
-        self.is_a = not self.is_a
-        self.button5.setDisabled(self.is_a)
+        self.button5_enable = not self.button5_enable
+        self.button5.setDisabled(self.button5_enable)
 
     def configure(self):
         """配置页面及控件属性, 要分清哪些是需要在重写之前，哪些是在重写之后哦"""
         self.bar_close = self.bar.btn_bar_close
         self.bar_mini = self.bar.btn_bar_min
         self.bar_normal = self.bar.btn_bar_normal
-        super(TestActivity, self).configure()
+        super(StartExampleButtonActivity, self).configure()
         self.bar.btn_bar_app_logo.setIcon(self.resource.qt_icon_project_png)
+        self.bar.btn_bar_app_name.setText("button 实例")
+
+        #  将button8 升格为派生子类 BaseButton 解锁更多新姿势。
         self.button8 = QPushButtonToBaseButton(self.button8)
         self.button8.setListenHover(True)
-        # self.button8.setDisabled(True)
-        self.button8.setProperty(*WidgetProperty.btn_class_normal[1])
+        self.button8.setProperty(*WidgetProperty.btn_class_danger[1])
+        # noinspection PyUnresolvedReferences
         self.button8.hover_in.connect(lambda: self.button8.setIcon(self.resource.awesome_font_icon("fa.grav")))
+        # noinspection PyUnresolvedReferences
         self.button8.hover_out.connect(lambda: self.button8.setIcon(self.resource.awesome_font_icon("fa.frown-o")))
 
 
 if __name__ == '__main__':
-    EasyQtInit(TestActivity()).run()
+    # 启动类
+    EasyQtInit(StartExampleButtonActivity()).run()
---
+++ example/view/start_example_button_ui.py
@@ -0,0 +1,102 @@
+# coding=utf-8
+"""
+    create by pymu
+    on 2020/12/31
+    at 9:37
+    使用EQ开发示例：使用qt designer ui模板生成的button
+"""
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget
+
+from common.base.button import QPushButtonToBaseButton
+from common.base.launch import EasyQtInit
+from config.const import WidgetProperty
+from view.activity.activity_dialog_normal import NormalDialogActivity
+from view.activity.activity_frame_less_window_hint import FrameLessWindowHintActivity
+from view.frame.frame_bar_demo0 import FrameBarDemo0
+from view.uipy.start_example_button import Ui_Form
+
+
+# noinspection PyAttributeOutsideInit
+class StartExampleButtonUIActivity(FrameLessWindowHintActivity, Ui_Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.procedure()
+
+    def place(self):
+        super(StartExampleButtonUIActivity, self).place()
+        # 添加默认的标题栏0
+        self.bar: FrameBarDemo0 = FrameBarDemo0(self)
+        self.body_layout.addWidget(self.bar, alignment=Qt.AlignTop)
+        content = QWidget()
+        self.setupUi(content)
+        self.body_layout.addWidget(content)
+        # 拉伸填充置顶
+        self.body_layout.addStretch()
+
+    # noinspection PyUnresolvedReferences
+    def set_signal(self):
+        super(StartExampleButtonUIActivity, self).set_signal()
+        self.pushButton_8.hover_in.connect(lambda: self.pushButton_8.setIcon(self.resource.awesome_font_icon("fa.grav")))
+        self.pushButton_8.hover_out.connect(lambda: self.pushButton_8.setIcon(self.resource.awesome_font_icon("fa.frown-o")))
+        self.pushButton_5.clicked.connect(lambda: NormalDialogActivity(info="当前按钮是可用的").exec_())
+
+
+    def configure(self):
+        """配置页面及控件属性, 要分清哪些是需要在重写之前，哪些是在重写之后哦"""
+        self.bar_close = self.bar.btn_bar_close
+        self.bar_mini = self.bar.btn_bar_min
+        self.bar_normal = self.bar.btn_bar_normal
+        super(StartExampleButtonUIActivity, self).configure()
+        self.bar.btn_bar_app_logo.setIcon(self.resource.qt_icon_project_png)
+        self.bar.btn_bar_app_name.setText("button 实例")
+
+        self.set_button_configure()
+
+    def set_button_configure(self):
+        """button配置"""
+        self.pushButton.setText("原始按钮")
+        self.pushButton.setToolTip("这是默认的按钮样式， 常用于表示取消、关闭之类不引起关注的按钮，与积极按钮生成对比效果更佳")
+
+        self.pushButton_2.setText("积极按钮")
+        self.pushButton_2.setToolTip("通常用于表示积极动作的按钮，比如确认、登录、注册、")
+        self.pushButton_2.setProperty(*WidgetProperty.btn_class_normal[1])
+
+        self.pushButton_3.setText("警告按钮")
+        self.pushButton_3.setToolTip("橙黄色表示警示，用于表示操作处于危险的边缘，也用于表示异常的状态")
+        self.pushButton_3.setProperty(*WidgetProperty.btn_class_warning[1])
+
+        self.pushButton_4.setText("危险按钮")
+        self.pushButton_4.setToolTip("红色警告，表示事件的严重性，常用于删除、销毁等动作")
+        self.pushButton_4.setProperty(*WidgetProperty.btn_class_danger[1])
+
+        self.pushButton_5.setText("禁止按钮")
+        self.pushButton_5.setToolTip("禁止操作，表示当前按钮不可用, 如果需要添加鼠标样式，"
+                                     "需要把QPushButton升格为BaseButton子类, 当前还是可以点击的，现在只是切换了样式")
+        self.pushButton_5.setProperty(*WidgetProperty.btn_class_disable[1])
+
+        self.pushButton_6.setText("带图标的按钮")
+        self.pushButton_6.setIcon(self.resource.qt_icon_project_png)
+        self.pushButton_6.setToolTip("给按钮添加图标")
+
+        # button 升格为BaseButton 子类
+        self.pushButton_7 = QPushButtonToBaseButton(self.pushButton_7)
+        self.pushButton_7.setText("被禁用的子类按钮")
+        self.pushButton_7.setIcon(self.resource.awesome_font_icon("fa.frown-o", color="red"))
+        self.pushButton_7.setEnabled(False)
+
+        self.pushButton_8.setText("可变图标的子类按钮")
+        self.pushButton_8.setIcon(self.resource.awesome_font_icon("fa.frown-o"))
+        self.pushButton_8 = QPushButtonToBaseButton(self.pushButton_8)
+        self.pushButton_8.setProperty(*WidgetProperty.btn_class_normal[1])
+        self.pushButton_8.setListenHover(True)
+
+        self.pushButton_9.setText("鼠标来在这里一下😶")
+        self.pushButton_9.setToolTip("<p>其实没那么丑, 只是布局的原因，被拉伸了</p>"
+                                     "<p>具体效果可以在流式布局中查看<span style='color:blue'>StartExampleButtonActivity</span></p>")
+
+
+if __name__ == '__main__':
+    # 启动类
+    EasyQtInit(StartExampleButtonUIActivity()).run()
